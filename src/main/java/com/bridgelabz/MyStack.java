a169b758efcab4b905646556a0b428cce605abbd
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-05-02 18:44:36 +0530

    Added : Creating Stack 56->30->70

--- src/main/java/com/bridgelabz/MyStack.java
+++ src/main/java/com/bridgelabz/MyStack.java
@@ -2,4 +2,25 @@ package com.bridgelabz;
 
 public class MyStack {
 
+    private final MyLinkedList myLinkedList;
+
+    //MyStack() create object of Linked List
+    public MyStack() {
+        this.myLinkedList = new MyLinkedList();
+    }
+
+    //push() push given Element in Stack by using Add method of Linked List
+    public void push(INode<Integer> newNode) {
+        myLinkedList.add(newNode);
+    }
+
+    //printStack() Display Nodes of Stack using printMyNodes method of Linked List
+    public void printStack() {
+        myLinkedList.printMyNodes();
+    }
+
+    //peak() peak first Element in Stack by using head field of Linked List
+    public INode peak() {
+        return myLinkedList.head;
+    }
 }
